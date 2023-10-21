2ad47bce1c323e363195eecfb19fa10ea57e9dac
mrzhu666 <1052712811@qq.com>
2023-10-21 19:16:16 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 
 # 清空文件夹
 sudo rm -rf /var/www/sql-father-cloud/*
+# 选择nodejs版本
+nvm use 16
 # 构建静态文件
 npm run build
 # 复制文件
-cp ./dist/. /var/www/sql-father-cloud
+sudo cp -r ./dist/. /var/www/sql-father-cloud
